b4b713a18432cb7c67a89b8f938977060f104d0f
zemerick <zemerick@emerickcc.com>
2019-02-19 14:45:17 -0600

    fixed bash scripts

--- scripts/shell_backup.sh
+++ scripts/shell_backup.sh
@@ -2,4 +2,5 @@
 # Since shell_command throws a fit when you call python3 directly for some reason, I use this script to call
 # a python script indirectly. This method works well.
 cd /home/homeassistant/.homeassistant/scripts
-python3 /home/homeassistant/.homeassistant/scripts/backup.py
+/usr/bin/scl enable rh-python36 bash
+/opt/rh/rh-python36/root/usr/bin/python3 /home/homeassistant/.homeassistant/scripts/backup.py
