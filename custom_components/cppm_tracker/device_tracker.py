03e12168828d59eb661cbd278e30d1436e92372e
zemerick <zemerick@emerickcc.com>
2019-03-07 13:20:03 -0600

    Added a lot here. Custom pyaarlo comp. Updated custom device tracker to be compatible with HA. PR request https://github.com/home-assistant/home-assistant/pull/21673

--- custom_components/cppm_tracker/device_tracker.py
+++ custom_components/cppm_tracker/device_tracker.py
@@ -1,57 +1,39 @@
 import logging
 from datetime import timedelta
 import voluptuous as vol
-import homeassistant.components as core
 from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
-from homeassistant.components.device_tracker import PLATFORM_SCHEMA, DeviceScanner, DOMAIN
-from homeassistant.components import device_tracker
+from homeassistant.components.device_tracker import (
+    PLATFORM_SCHEMA, DeviceScanner, DOMAIN
+)
 from homeassistant.const import (
-    STATE_HOME, STATE_NOT_HOME, CONF_HOST, CONF_API_KEY, CONF_SCAN_INTERVAL, CONF_USERNAME
+    CONF_HOST, CONF_API_KEY, CONF_SCAN_INTERVAL
 )
 
 REQUIREMENTS = ['clearpasspy==1.0.2']
 
-SCAN_INTERVAL = timedelta(seconds=120)
-CONF_USERNAME = 'client_id'
+CONF_SCAN_INTERVAL = timedelta(seconds=120)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
-    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required('client_id'): cv.string,
     vol.Required(CONF_API_KEY): cv.string,
-    vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL): cv.time_period
 })
 
-
-DOMAIN ="device_tracker"
-
-
 _LOGGER = logging.getLogger(__name__)
-_LOGGER.setLevel(logging.DEBUG)
-_LOGGER.addHandler(logging.StreamHandler())
 
-CONF_HOST = 'host'
-CONF_API_KEY = 'api_key'
-CONF_SCAN_INTERVAL= 'scan_interval'
-
-async def get_scanner(hass, config):
-    cppm_host = config[DOMAIN].get(CONF_HOST)
-    api_key = config[DOMAIN].get(CONF_API_KEY)
-    client_id = config[DOMAIN].get(CONF_USERNAME)
-    scan_interval = config[DOMAIN].get(CONF_SCAN_INTERVAL)
+def get_scanner(hass, config):
     grant_type = 'client_credentials'
-    newScan = CPPMDeviceScanner(hass, cppm_host, client_id, api_key, grant_type, scan_interval)
-    return newScan if newScan.success_init else None
+    new_scan = CPPMDeviceScanner(config[DOMAIN], grant_type)
+    return new_scan if new_scan.success_init else None
 
 class CPPMDeviceScanner(DeviceScanner):
-    def __init__(self, hass, cppm_host, client_id, api_key, grant_type, scan_interval):
+    def __init__(self, config, grant_type):
         _LOGGER.debug("-------------INIT CALLED--------------")
-        self._hass = hass
-        self._cppm_host = cppm_host
-        self._api_key = api_key
+        self._cppm_host = config[CONF_HOST]
+        self._api_key = config[CONF_API_KEY]
         self._grant_type = grant_type
-        self._client_id = client_id
-        self._scan_int = scan_interval
+        self._client_id = config['client_id']
         self.success_init = self.get_cppm_data()
 
     async def async_scan_devices(self):
@@ -62,8 +44,8 @@ class CPPMDeviceScanner(DeviceScanner):
     async def async_get_device_name(self, device):
         _LOGGER.debug("------ RESOLVING DEVICE NAME ----")
         return [device['name'] for device in self.results]
-    @Throttle(SCAN_INTERVAL)
 
+    @Throttle(CONF_SCAN_INTERVAL)
     def get_cppm_data(self):
         """Retrieve data from Aruba Clearpass and return parsed result."""
         from clearpasspy import ClearPass
@@ -74,13 +56,13 @@ class CPPMDeviceScanner(DeviceScanner):
             'secret': self._api_key,
             'client': self._client_id
         }
-        _LOGGER.debug("DATA: {}".format(data))
+        _LOGGER.debug("DATA: %s", data)
 
         CPPM = ClearPass(data)
         endpoints = CPPM.get_endpoints(100)['_embedded']['items']
         devices = []
         for item in endpoints:
-            if CPPM.online_status(item['mac_address']) == True:
+            if CPPM.online_status(item['mac_address']):
                 device = {
                     'mac': item['mac_address'],
                     'name': item['mac_address']
@@ -89,5 +71,6 @@ class CPPMDeviceScanner(DeviceScanner):
             else:
                 continue
         _LOGGER.debug("-----------Update successful!-----------")
+        _LOGGER.debug("Devices: %s", devices)
         self.results = devices
         return True
